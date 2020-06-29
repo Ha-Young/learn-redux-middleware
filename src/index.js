573c29ad35f6f66efb99e2ac0ccaa0d86b6a8284
Ha0 <hayeong28@naver.com>
2020-06-29 17:10:06 +0900

    yarn add: redux-devtools-extension && add: apply redux devtools

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
