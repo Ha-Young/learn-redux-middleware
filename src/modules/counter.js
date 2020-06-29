db163b89d724d454492f22d58ca01164ac7a035a
Ha0 <hayeong28@naver.com>
2020-06-29 16:40:55 +0900

    prepare project before learn to redux-middleware

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,5 +1,5 @@
-const INCREASE = "INCREASE";
-const DECREASE = "DECREASE";
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
