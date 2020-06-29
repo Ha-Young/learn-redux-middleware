ee4f28370dba335ba535d5b44717dea754865f02
Ha0 <hayeong28@naver.com>
2020-06-29 18:29:31 +0900

    make reducer at posts

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -71,3 +71,79 @@ export const getPost = () => async (dispatch) => {
     dispatch(postError(e));
   }
 };
+
+const initialState = {
+  posts: {
+    loading: false,
+    data: null,
+    error: null,
+  },
+  post: {
+    loading: false,
+    data: null,
+    error: null,
+  },
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: {
+          loading: true,
+          data: null,
+          error: null,
+        },
+      };
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: {
+          loading: false,
+          data: action.posts,
+          error: null,
+        },
+      };
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: {
+          loading: false,
+          data: null,
+          error: action.error,
+        },
+      };
+
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          loading: true,
+          data: null,
+          error: null,
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          loading: false,
+          data: action.post,
+          error: null,
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          loading: false,
+          data: null,
+          error: action.error,
+        },
+      };
+
+    default:
+      return state;
+  }
+}
