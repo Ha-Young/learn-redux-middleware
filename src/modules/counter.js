38c251eb4b359a3d0888663521e1caf3ead241db
Ha0 <hayeong28@naver.com>
2020-06-29 17:46:09 +0900

    learn redux thunk - redux thunk is possible to use functional parameter in dispatch function

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -4,6 +4,18 @@ const DECREASE = "counter/DECREASE";
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+export const increaseAsync = () => (dispatch, getState) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch, getState) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0;
 
 export default function counter(state = initialState, action) {
