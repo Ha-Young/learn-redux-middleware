654dab4a404d3d8cc7e6dec456b2afbfe773b9cb
hychoi <hayeong28@naver.com>
2020-07-26 15:34:08 +0900

    change: thunk -> saga step1 - thunk function to action creator function

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,21 +1,16 @@
+// 액션 타입
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
+const INCREASE_ASYNC = "counter/INCREASE_ASYNC";
+const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
 
+// 액션 생성 함수 (thunk를 일반 액션생성함수로)
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-export const increaseAsync = () => (dispatch, getState) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-
-export const decreaseAsync = () => (dispatch, getState) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
-
+// 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없음)
 const initialState = 0;
 
 export default function counter(state = initialState, action) {
