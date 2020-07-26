267a46a638b8340d986a12137f2af6c8cb34ca1d
hychoi <hayeong28@naver.com>
2020-07-26 15:37:19 +0900

    change: thunk -> saga step2 - make saga && learn delay, put function in redux-saga/effects

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,3 +1,6 @@
+import { delay, put } from "redux-saga/effects"; // redux-saga의 effects에는 다양한 유틸함수들이 들어있다.
+// 여기서 put이 제일 중요한데, 새로운 액션을 디스패치 할 수 있다.
+
 // 액션 타입
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
@@ -9,6 +12,18 @@ export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 export const increaseAsync = () => ({ type: INCREASE_ASYNC });
 export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
+
+// increaseSaga와 decreaseSaga를 다음과 같이 생성.
+// redux-saga에서는 제너레이터 함수를 "사가" 라고 부른다.
+function* increaseSaga() {
+  yield delay(1000); // 1초 대기
+  yield put(increase()); // put은 특정 액션을 디스패치 해준다.
+}
+
+function* decreaseSaga() {
+  yield delay(1000); // 1초 대기
+  yield put(decrease()); // put은 특정 액션을 디스패치 해준다.
+}
 
 // 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없음)
 const initialState = 0;
